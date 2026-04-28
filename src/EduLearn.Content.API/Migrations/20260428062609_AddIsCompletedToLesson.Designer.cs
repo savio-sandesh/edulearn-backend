8fe538bc97d9b5cd9999f6f5114809e329b057e4
savio-sandesh <sandesh.cse00@gmail.com>
2026-04-28 12:17:23 +0530

    [Sandesh].Refactor - IsCompleted column to Lessons table and update migrations

--- src/EduLearn.Content.API/Migrations/20260428062609_AddIsCompletedToLesson.Designer.cs
+++ src/EduLearn.Content.API/Migrations/20260428062609_AddIsCompletedToLesson.Designer.cs
@@ -4,6 +4,7 @@ using EduLearn.Content.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EduLearn.Content.API.Migrations
 {
     [DbContext(typeof(ContentDbContext))]
-    partial class ContentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260428062609_AddIsCompletedToLesson")]
+    partial class AddIsCompletedToLesson
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/EduLearn.Content.API/Migrations/20260428062609_AddIsCompletedToLesson.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduLearn.Content.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCompletedToLesson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Lessons",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Lessons");
+        }
+    }
+}
